3a638ff5772f9259189252a4ed883a56f98b31c9
Sergey Sova <mail@sergeysova.com>
2022-09-06 15:48:02 +0400

    On branch main

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sova.dev',
+  integrations: [react(), tailwind(), sitemap(), prefetch(), mdx()]
+});
