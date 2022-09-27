98ef4d75ead0859304bf3b1a41f46e8045645026
Sergey Sova <mail@sergeysova.com>
2022-09-27 16:30:55 +0400

    Add LinkTo with links index

--- src/lib/language.ts
+++ src/lib/language.ts
@@ -5,3 +5,8 @@ const current = import.meta.env.PUBLIC_LANGUAGE as LANG;
 export function switchLang<T>(variants: {[Key in LANG]: T}): T {
   return variants[current];
 }
+
+export function optionalTranslate<T extends string | number>(variants: {[Key in LANG]: T} | T) {
+  if (typeof variants === 'object' && variants !== null) return switchLang(variants);
+  return variants;
+}
